9e154c3fb79fc5e4412591d3b9114c11b0e008dd
rahulMukherjee24 <rahul.mukherjee8824@gmail.com>
2025-07-01 11:45:12 +0530

    initial commit- remaining files

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDfpcRAK6FK0Mj2y2n12fXVhAHEPxJViGk',
+    authDomain: 'classicimpressionphotoprinting.firebaseapp.com',
+    projectId: 'classicimpressionphotoprinting',
+    storageBucket: 'classicimpressionphotoprinting.firebasestorage.app',
+    messagingSenderId: '112540812007',
+    appId: '1:112540812007:web:ee54dfd1c99fc62b55d842',
+    measurementId: 'G-LR9CW3SFRH',
+  },
+};
